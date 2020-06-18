06f4af5f0bb3a8eee905a99d244250386efc6b0a
shindo <727792353@11.com>
2020-06-18 18:00:04 +0800

    add async

---
+++ src/main/java/shindo/Java/async/AsyncCall.java
@@ -0,0 +1,31 @@
+package shindo.Java.async;
+
+import org.bouncycastle.jce.provider.symmetric.Grainv1;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 主要验证异步转同步的5种方法：
+ * 本类为异步调用模型
+ */
+public class AsyncCall {
+    private Random random = new Random(System.currentTimeMillis());
+
+    private ExecutorService tp = Executors.newSingleThreadExecutor();
+
+    //demo 1，2，4，5的调用方法
+    public void call(BaseDemo demo){
+        new Thread(()->{
+            long res = random.nextInt(10);
+            try {
+                Thread.sleep(res*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            demo.callback(res);
+        }).start();
+    }
+
+}
---
+++ src/main/java/shindo/Java/async/BaseDemo.java
@@ -0,0 +1,13 @@
+package shindo.Java.async;
+
+public abstract class BaseDemo {
+    protected AsyncCall asyncCall = new AsyncCall();
+
+    public abstract void callback(long response);
+
+    public void call(){
+        System.out.println("发起调用");
+        asyncCall.call(this);
+        System.out.println("调用返回");
+    }
+}
